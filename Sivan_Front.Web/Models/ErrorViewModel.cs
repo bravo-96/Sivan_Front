035320c864ded33ee74ef540fff1968f62986105
bravo-96 <joseluis.bravo96@hotmail.com>
2022-10-13 14:51:29 -0300

    init

---
+++ Sivan_Front.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sivan_Front.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
